4df081f16b619f774cd5a2aa87d1a9ae54c16aa1
Luiz <luiizperren@hotmail.com>
2020-03-05 13:01:53 +0100

    Classes added. GUI angepasst
    Handler hinzugefügt. Noch kein EventListener eingefügt

--- GUI/SendOverUART.py
+++ GUI/SendOverUART.py
@@ -1,10 +1,13 @@
 import serial
 
-#ser = serial.Serial(9600)
+ser = serial.Serial(9600)
 
-#def sendOverUART(data):
-    #ser.write(data)
+def sendOverUART(data):
+    ser = serial.Serial ("/dev/ttyAMA0")    #Open named port 
+    ser.baudrate = 9600                     #Set baud rate to 9600                     
+    ser.write(data)                         #Send back the received data
+    ser.close()   
 
 def saveData(gesch, winkel):
     data = [gesch, winkel]
-    print(data)
+    sendOverUART(data)
