c41b99beed0fa68115612840bb1f3fe69d5ab624
Luiz <luiizperren@hotmail.com>
2020-03-12 10:58:34 +0100

    Corrected port

--- GUI/SendOverUART.py
+++ GUI/SendOverUART.py
@@ -1,7 +1,7 @@
 import serial
 
 def sendOverUART(gesch1, gesch2):
-    ser = serial.Serial ("/dev/ttyAMA0", 57600)    #Open named port     
+    ser = serial.Serial ("/dev/ttyS0", 57600)    #Open named port     
     ser.write(gesch1.encode())
     ser.write(gesch2.encode())                        #Send back the received data
     ser.close()
