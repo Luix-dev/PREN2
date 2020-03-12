8996a4fa03d65f60d701c8df104918327446ea84
Silvan Loser <silvan.loser@stud.hslu.ch>
2020-03-12 08:30:21 +0100

    trailing whitespaces

--- GUI/SendOverUART.py
+++ GUI/SendOverUART.py
@@ -3,10 +3,10 @@ import serial
 ser = serial.Serial(9600)
 
 def sendOverUART(data):
-    ser = serial.Serial ("/dev/ttyAMA0")    #Open named port 
-    ser.baudrate = 9600                     #Set baud rate to 9600                     
+    ser = serial.Serial ("/dev/ttyAMA0")    #Open named port
+    ser.baudrate = 9600                     #Set baud rate to 9600
     ser.write(data)                         #Send back the received data
-    ser.close()   
+    ser.close()
 
 def saveData(gesch, winkel):
     data = [gesch, winkel]
